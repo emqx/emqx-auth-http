93057892e56b72fe91098c35211b1ec61476f2d7
zhouzb <zhouzb@emqx.io>
2020-12-04 14:47:16 +0800

    fix: fix the issue that flush only one message

--- src/emqx_http_client.erl
+++ src/emqx_http_client.erl
@@ -245,12 +245,12 @@ do_request(Client, post, {Path, Headers, Body}) ->
 
 flush_stream(Client, StreamRef) ->
     receive
-		{gun_response, Client, StreamRef, _, _, _} ->
-			ok;
-		{gun_data, Client, StreamRef, _, _} ->
-			ok;
-		{gun_error, Client, StreamRef, _} ->
-			ok
+        {gun_response, Client, StreamRef, _, _, _} ->
+            flush_stream(Client, StreamRef);
+        {gun_data, Client, StreamRef, _, _} ->
+            flush_stream(Client, StreamRef);
+        {gun_error, Client, StreamRef, _} ->
+            flush_stream(Client, StreamRef)
 	after 0 ->
 		ok
 	end.
