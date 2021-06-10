59b801830b2317b2f21a9f6e721eca7a993ea3e2
zhouzb <zhouzb@emqx.io>
2021-06-10 18:29:35 +0800

    fix(webhook): support query string parameter

--- src/emqx_auth_http_app.erl
+++ src/emqx_auth_http_app.erl
@@ -128,13 +128,12 @@ translate_env() ->
                         Env ->
                             URL = proplists:get_value(url, Env),
                             #{host := Host,
-                              path := Path,
                               scheme := Scheme} = URIMap = uri_string:parse(add_default_scheme(URL)),
                             Port = maps:get(port, URIMap, case Scheme of
                                                               "https" -> 443;
                                                               _ -> 80
                                                           end),
-                            [{Name, {Host, Port, path(Path)}} | Acc]
+                            [{Name, {Host, Port, path(URIMap)}} | Acc]
                     end
                 end, [], [acl_req, auth_req, super_req]),
     case same_host_and_port(URLs) of
@@ -163,8 +162,14 @@ add_default_scheme("https://" ++ _ = URL) ->
 add_default_scheme(URL) ->
     "http://" ++ URL.
 
-path("") -> "/";
-path(Path) -> Path.
+path(#{path := "", 'query' := Query}) ->
+    "?" ++ Query;
+path(#{path := Path, 'query' := Query}) ->
+    Path ++ "?" ++ Query;
+path(#{path := ""}) ->
+    "/";
+path(#{path := Path}) ->
+    Path.
 
 same_host_and_port([_]) ->
     true;
